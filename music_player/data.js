c56277d1dcf156096151ee6b27790401aebaee5a
Divyagoyal3 <Divyagoyal3>
2023-06-11 19:43:50 +0530

    1st day with music

---
+++ music_player/data.js
@@ -0,0 +1,32 @@
+let songs = [
+    {
+    name :'I m mess',
+    path:'music/I m mess.mp3',
+    artist:'artist 1',
+    cover:'images/cover1.png'
+},
+{
+    name :'On my way',
+    path:'music/On my way.mp3',
+    artist:'artist 2',
+    cover:'images/cover2.png'
+},
+{
+    name :'Play',
+    path:'music/Play.mp3',
+    artist:'artist 3',
+    cover:'images/cover3.png'
+},
+{
+    name :'We Donot talk Anymore',
+    path:'music/We Donot talk Anymore.mp3',
+    artist:'artist 4',
+    cover:'images/cover4.png'
+},
+{
+    name :'Darkside',
+    path:'music/Darkside.mp3',
+    artist:'artist 5',
+    cover:'images/cover5.png'
+},
+]
